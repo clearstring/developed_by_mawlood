16117db991abfa76fe522c78ee53d6249e52eeaa
clearstring <imepic12341@gmail.com>
2025-04-06 03:42:39 +0800

    Update 4.2loader.lua

--- 4.2loader.lua
+++ 4.2loader.lua
@@ -1,4 +1,4 @@
-if not (loadstring and getrenv and getsenv and debug and debug.setstack and debug.getstack) then game:GetService("Players").LocalPlayer:Kick("executor not supported\nUse a swift, atlantis or velocity, they are all free!!!\ninvites:\ndiscord.gg/getatlantis\nhttps://discord.gg/getswift\ndiscord.gg/getvelocity") end
+if not (loadstring and getrenv and getsenv and debug.setstack and debug.getstack) then game:GetService("Players").LocalPlayer:Kick("executor not supported\nUse swift, atlantis or velocity, they are all free!!!\ninvites:\ndiscord.gg/getatlantis\ndiscord.gg/getswift\ndiscord.gg/getvelocity") end
 
 
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/abc7588fdaccdfee512230d3c6f605f6.lua"))()
